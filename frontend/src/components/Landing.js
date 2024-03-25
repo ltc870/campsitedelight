dc417b76889984e9642673b86bf252685a434de6
Lawrence C <lawrencecaudle870@gmail.com>
2024-03-24 23:36:16 -0400

    made updates to landing page

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,13 +1,33 @@
-import React from "react";
+import { useState } from "react";
+import Register from "./Register";
+import Login from "./Login";
 
 const Landing = () => {
+  const [showRegister, setShowRegister] = useState(true);
+
+  const handleRegisterClick = () => {
+    setShowRegister(true);
+  };
+
+  const handleLoginClick = () => {
+    setShowRegister(false);
+  };
+
   return (
     <header className="header">
       <h1 className="header-title">Campsite Delight</h1>
       <section className="header-buttons">
-        <div className="header-btn header-btn-register">Register</div>
-        <div className="header-btn header-btn-login">Login</div>
+        <div
+          className="header-btn header-btn-register"
+          onClick={handleRegisterClick}
+        >
+          Register
+        </div>
+        <div className="header-btn header-btn-login" onClick={handleLoginClick}>
+          Login
+        </div>
       </section>
+      {showRegister ? <Register /> : <Login />}
     </header>
   );
 };
