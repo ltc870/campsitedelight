c9d25cc25aa3d8f86b2ed2de6780a06cddd1f5ce
Lawrence C <lawrencecaudle870@gmail.com>
2024-03-26 22:02:03 -0400

    added more components and registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Dashboard";
 // import Login from "./components/Login";
-// import Register from "./components/Register";
+import Register from "./components/Register";
 import Landing from "./components/Landing";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" exact element={<Landing />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,7 +1,111 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "./Spinner";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { name, email, password, password2 } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) toast.error(message);
+    if (isSuccess) {
+      navigate("/dashboard");
+    }
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      toast.error("Passwords do not match");
+    } else {
+      const userData = { name, email, password };
+      dispatch(register(userData));
+    }
+  };
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div>
+      <section>
+        <p>Please create an account</p>
+      </section>
+      <section>
+        <form onSubmit={onSubmit}>
+          <div>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Enter your name"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Confirm your password"
+              onChange={onChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default Register;
